6f16c3c485dc93eb7b2e5ae2a0b2d833e7b8add7
Delfio <delfio_eu@hotmail.com>
2020-07-05 17:42:36 -0400

    integrating with unform

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FiArrowLeft, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
 import LogoImg from '../../asssets/logo.svg';
 
 import Button from '../../components/button';
@@ -7,28 +9,34 @@ import Input from '../../components/input';
 
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={LogoImg} alt="logo go barber" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={LogoImg} alt="logo go barber" />
 
-      <form>
-        <h1>Faça seu Cadastro</h1>
-        <Input icon={FiUser} name="name" placeholder="seu nome" type="text" />
-        <Input icon={FiUser} name="email" placeholder="seu e-mail" type="email" />
-        <Input icon={FiLock} type="password" placeholder="senha" name="password" />
-        <Input icon={FiLock} type="password" placeholder="confirme sua senha" name="confirmPassword" />
-        <Button type="submit">Cadastrar</Button>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input icon={FiUser} name="name" placeholder="seu nome" type="text" />
+          <Input icon={FiUser} name="email" placeholder="seu e-mail" type="email" />
+          <Input icon={FiLock} type="password" placeholder="senha" name="password" />
+          <Input icon={FiLock} type="password" placeholder="confirme sua senha" name="confirmPassword" />
+          <Button type="submit">Cadastrar</Button>
 
-      </form>
+        </Form>
 
-      <a href="createaccont">
-        <FiArrowLeft size={24} color="white" />
-        Já tenho cadastro
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="createaccont">
+          <FiArrowLeft size={24} color="white" />
+          Já tenho cadastro
+        </a>
+      </Content>
+    </Container>
+
+  );
+};
 
 export default SignUp;
