d076884ee08bdc056762fa5072a7c18f8d925aec
Delfio <delfio_eu@hotmail.com>
2020-07-05 18:22:01 -0400

    validation using yup

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useCallback } from 'react';
 import {
   FiArrowLeft, FiUser, FiLock, FiMail,
 } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import LogoImg from '../../asssets/logo.svg';
 
@@ -12,9 +13,23 @@ import Input from '../../components/input';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schemaValidation = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('Email obrigatório').email('E-mail inválido'),
+        password: Yup.string().min(6, 'Minímo de 6 caractéres'),
+        confirmPassword: Yup.string().required('Senha obrigatória').min(6, 'Minímo de 6 caractéres'),
+      });
+
+      await schemaValidation.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <Container>
       <Background />
