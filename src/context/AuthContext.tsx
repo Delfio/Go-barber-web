f0d32f485a1810b4acf0be40921897e9f4d746e0
Delfio <delfio_eu@hotmail.com>
2020-07-16 21:36:33 -0400

    saving login information in context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/apiClient';
 
 interface SignInCredentials {
@@ -7,24 +7,57 @@ interface SignInCredentials {
 }
 
 interface IAuthContext {
-    name: string;
+    user: IUser;
     signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+interface IUser {
+    id: string;
+    name: string;
+    email: string;
+    avatar: any;
+}
+
+interface IRequestData{
+    User: IUser;
+    token: string;
+}
+
+interface IAuthState {
+  token: string;
+  user: IUser;
 }
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext);
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<IAuthState>(() => {
+    const token = localStorage.getItem('@Gobarber:token');
+    const user = localStorage.getItem('@Gobarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as IAuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
-    const response = await api.post('/sessions', {
+    const response = await api.post<IRequestData>('/sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, User } = response.data;
+
+    localStorage.setItem('@Gobarber:token', token);
+    localStorage.setItem('@Gobarber:user', JSON.stringify(User));
+
+    setData({ token, user: User });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Delfio', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
