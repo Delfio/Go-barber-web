32df5b10073945cc295dc6584f1aa0a2b64a10e6
Delfio <delfio_eu@hotmail.com>
2020-07-19 20:01:26 -0400

    navigation validation between private routes

---
+++ src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { RouteProps as ReactDomRouteProps, Route as ReactDomRoute, Redirect } from 'react-router-dom';
+import { useAuth } from '../hooks/Auth';
+// import { Container } from './styles';
+
+interface RouteProps extends ReactDomRouteProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+/**
+ * Tabela verdade - condição and 'Autenticado' 'Rota privada'
+ * true / true = ok
+ * true / false = redirecionar
+ * false / true = redirecionar
+ * false / false = ok
+ */
+
+const Route: React.FC<RouteProps> = ({ isPrivate = false, component: Component, ...rest }) => {
+  const { user } = useAuth();
+
+  console.log(user);
+
+  return (
+    <ReactDomRoute
+      {...rest}
+      render={({ location }) => (isPrivate === !!user ? (
+        <Component />
+      ) : (
+        <Redirect to={{
+          pathname: isPrivate ? '/' : '/dashboard',
+          state: {
+            from: location,
+          },
+        }}
+        />
+      ))}
+    />
+  );
+};
+
+export default Route;
