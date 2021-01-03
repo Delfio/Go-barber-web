fb391721f5284a8b6ff564627e0e9f15b76693aa
Delfio Francisco <delfio_eu@hotmail.com>
2021-01-03 17:11:29 -0400

    Estilização inicial do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Schedule,
+  Content,
+  NextAppointment,
+  Calendar,
+} from './styles';
+import logoImage from '../../asssets/logo.svg';
+import { useAuth } from '../../hooks/Auth';
 
-const Dashboard: React.FC = () => (
-  <h1>Dashboard</h1>
-);
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImage} alt="GoBarber" />
+
+          <Profile>
+            <img
+              src={user.avatar}
+              alt={user.name}
+            />
+            <div>
+              <span>Bem-Vindo</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-Feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/42546922?s=460&u=7e7ee3bc668ac96dab3ed6f5653ff1940413cc7b&v=4"
+                alt="Delfio Francisco"
+              />
+              <strong>Delfio Francisco</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
+    </Container>
+  );
+};
 
 export default Dashboard;
